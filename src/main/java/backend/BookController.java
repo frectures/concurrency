45d7924638c67c8045135ad30e692e9635aee0b5
frectures <frectures@users.noreply.github.com>
2025-05-16 15:44:28 +0200

    BookController

--- src/main/java/backend/BookController.java
+++ src/main/java/backend/BookController.java
@@ -1,34 +1,37 @@
 package backend;
 
+import io.vavr.collection.Vector;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.atomic.AtomicReference;
 
 @RestController
 @RequestMapping("/books")
 public class BookController {
 
-    private final List<Book> books = new ArrayList<>();
+    private AtomicReference<Vector<Book>> books = new AtomicReference(Vector.of(
+            new Book("Effective Java", "Joshua Bloch", 2017),
+            new Book("Java Concurrency in Practice", "Brian Goetz", 2006)
+    ));
 
     public BookController() {
-        books.add(new Book("Effective Java", "Joshua Bloch", 2017));
-        books.add(new Book("Java Concurrency in Practice", "Brian Goetz", 2006));
     }
 
     @GetMapping
     public Iterable<Book> getAllBooks() {
-        return books;
+        return books.get();
     }
 
     @GetMapping("/{index}")
     public Book getBookAt(@PathVariable("index") int index) {
-        return books.get(index);
+        return books.get().get(index);
     }
 
     @PostMapping
     public Iterable<Book> addBook(@RequestBody Book newBook) {
-        books.add(newBook);
-        return books;
+        return books.updateAndGet(v -> v.append(newBook));
     }
 }
