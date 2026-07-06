133278448c57a51f37e3b37c472a53472e98447e
frectures <frectures@users.noreply.github.com>
2026-07-06 12:43:55 +0200

    replace Benchmark.java with JMH

--- src/main/java/live/Sum.java
+++ src/main/java/live/Sum.java
@@ -1,70 +1,71 @@
 package live;
 
+import org.openjdk.jmh.annotations.*;
+
+import java.io.IOException;
 import java.util.Arrays;
-import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@Warmup(iterations = 2, time = 1, timeUnit = TimeUnit.SECONDS)
+@Measurement(iterations = 3, time = 1, timeUnit = TimeUnit.SECONDS)
+@Fork(1)
+@State(Scope.Benchmark)
 public class Sum {
-    private static final int PROBLEM_SIZE = 500_000_000;
+    private final int[] a;
+    private final int expectedSum;
 
-    static void main() {
-        int[] a = new int[PROBLEM_SIZE];
-        randomize(a);
-        Benchmark benchmark;
-
-        benchmark = new Benchmark("sequential loop");
-        while (benchmark.requiresMoreMeasurements()) {
-            benchmark.measure(() -> sequentialLoop(a));
-        }
-
-        benchmark = new Benchmark("sequential stream");
-        while (benchmark.requiresMoreMeasurements()) {
-            benchmark.measure(() -> sequentialStream(a));
-        }
-
-        benchmark = new Benchmark("parallel loop");
-        while (benchmark.requiresMoreMeasurements()) {
-            benchmark.measure(() -> parallelLoop(a));
-        }
-
-        benchmark = new Benchmark("parallel stream");
-        while (benchmark.requiresMoreMeasurements()) {
-            benchmark.measure(() -> parallelStream(a));
-        }
-    }
-
-    private static void randomize(int[] a) {
-        System.out.println("randomizing...");
-        Random random = new Random(0);
+    public Sum() {
+        a = new int[100_000_000];
+        System.out.print("RANDOMIZING... ");
+        long seed = 0;
+        int sum = 0;
         for (int i = 0; i < a.length; ++i) {
-            a[i] = random.nextInt(90) + 10;
+            seed = seed * 6364136223846793005L + 1;
+            sum += a[i] = (int) (seed >>> 32);
         }
+        expectedSum = sum;
     }
 
-    private static int sequentialLoop(int[] a) {
+    private int check(int sum) {
+        if (sum != expectedSum) throw new RuntimeException(sum + " != " + expectedSum);
+        return sum;
+    }
+
+    @Benchmark
+    public int sequentialLoop() {
         int sum = 0;
         for (int x : a) {
             sum += x;
         }
-        return sum;
+        return check(sum);
     }
 
-    private static int sequentialStream(int[] a) {
-        return Arrays.stream(a).sum();
+    @Benchmark
+    public int sequentialStream() {
+        return check(Arrays.stream(a).sum());
     }
 
-    private static int parallelLoop(int[] a) throws InterruptedException {
+    @Benchmark
+    public int parallelLoop() throws InterruptedException {
         int sum = 0;
         // TODO parallelize
-        for (int i = 0; i < PROBLEM_SIZE / 2; ++i) {
+        for (int i = 0; i < a.length / 2; ++i) {
             sum += a[i];
         }
-        for (int i = PROBLEM_SIZE / 2; i < PROBLEM_SIZE; ++i) {
+        for (int i = a.length / 2; i < a.length; ++i) {
             sum += a[i];
         }
-        return sum;
+        return check(sum);
     }
 
-    private static int parallelStream(int[] a) {
-        return Arrays.stream(a).parallel().sum();
+    @Benchmark
+    public int parallelStream() {
+        return check(Arrays.stream(a).parallel().sum());
+    }
+
+    static void main() throws IOException {
+        org.openjdk.jmh.Main.main(new String[]{Sum.class.getName()});
     }
 }
