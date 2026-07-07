ec36164228bca5e6ebe6f98849e861c68a6bfc74
frectures <frectures@users.noreply.github.com>
2026-07-07 10:35:52 +0200

    rename BookApplication to DemoApplication

--- src/main/java/backend/DemoApplication.java
+++ src/main/java/backend/DemoApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BookApplication {
+public class DemoApplication {
     static void main(String[] args) {
-        SpringApplication.run(BookApplication.class, args);
+        SpringApplication.run(DemoApplication.class, args);
     }
 }
